ae843dcfa66c385a5c616ca2147b694757789063
unknown <ahmadktb2323@gmailcom>
2025-05-30 21:59:36 +0700

    membuat echo pada php folder sintaks

--- sintaks-php/index.php
+++ sintaks-php/index.php
@@ -3,6 +3,10 @@
 
 
 echo "hello world";
+
+echo "ini materi pengenalan pada belajar bahasa pemrogramman php";
+
+echo "di php ; ini sangat penting";
 // ini comment in php
 
 #ini komentar juga di php
