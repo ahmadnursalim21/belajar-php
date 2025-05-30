0cabec6e5605309cdbb967b30365f5873677bd5e
unknown <ahmadktb2323@gmailcom>
2025-05-30 22:04:20 +0700

    membuat code html pada folder sintaks file index.php

--- sintaks-php/index.php
+++ sintaks-php/index.php
@@ -4,9 +4,15 @@
 
 echo "hello world";
 
+echo "<br>";
+
 echo "ini materi pengenalan pada belajar bahasa pemrogramman php";
+echo "<br>";
 
 echo "di php ; ini sangat penting";
+echo "<br>";
+
+
 // ini comment in php
 
 #ini komentar juga di php
@@ -15,3 +21,25 @@ echo "di php ; ini sangat penting";
 komentar 
 tapi banyak 
 baris */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+
+<body>
+
+    <p>di php bisa membuat code html juga</p>
+
+    <p><?php echo "nama saya adalah ahmad nur salim" ?></p>
+    <p><?= "ini contoh kedua nya pak" ?></p>
+
+</body>
+
+</html>
