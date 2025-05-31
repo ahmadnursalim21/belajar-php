5ab3a2498907b316e38631fa22dab612ed972129
unknown <ahmadktb2323@gmailcom>
2025-05-31 13:34:33 +0700

    membuat folder looping dan belajar looping difile index.php

---
+++ looping/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Looping in PHP</title>
+</head>
+
+<body>
+    <h1>belajar looping di php</h1>
+    <?php
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<p>Ini adalah perulangan ke-$i</p>";
+    }
+    ?>
+</body>
+
+</html>
